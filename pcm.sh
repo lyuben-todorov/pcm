2266d578f9ec9ea0c6278051eb73d2cc902700c5
Lyuben Todorov <lyuben.todorov@gmail.com>
2020-08-12 21:24:46 +0300

    Fixed and detachable.

--- pcm.sh
+++ pcm.sh
@@ -2,8 +2,20 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO="git@github.com:lyuben-todorov/piko.git"
-git clone	"$REPO" "$DIR/piko"
+CONFIG=configs/*
 
-cd piko
-cargo build --release
+if [ ! -d "/piko" ]
+then
+	git clone "$REPO" "$DIR/piko"
+	cd piko && cargo build --release
+	cd ..
+fi
+
+
+for f in $CONFIG
+do
+	echo "Starting $f"
+	
+	./piko/target/release/main -p "$DIR/$f" & disown
+done
 
