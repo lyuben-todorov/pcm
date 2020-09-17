2fa7deaeec1d026f3110e7d92ff9470ec37ee2f9
Lyuben Todorov <lyuben.todorov@gmail.com>
2020-09-17 20:56:05 +0300

    Update pcm

--- pcm.sh
+++ pcm.sh
@@ -4,18 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO="git@github.com:lyuben-todorov/piko.git"
 CONFIG=configs_enabled/*
 
-if [ ! -d "/piko" ]
-then
-	git clone "$REPO" "$DIR/piko"
-	cd piko && cargo build --release
-	cd ..
-fi
-
 export RUST_BACKTRACE=1
+cd ../node && cargo build --release
 
 for f in $CONFIG
 do
 	echo "Starting $f"
-	./piko/target/release/main -p "$DIR/$f"
+	./target/release/main -p "$DIR/$f"
 done
 
