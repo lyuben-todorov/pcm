7899350861dff9657e763af7f617a24feb245bb6
Lyuben Todorov <lyuben.todorov@gmail.com>
2020-09-22 19:27:35 +0300

    Fix deploy script

--- pcm.sh
+++ pcm.sh
@@ -2,14 +2,14 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO="git@github.com:lyuben-todorov/piko.git"
-CONFIG=configs_enabled/*
+CONFIG_FOLDER=$DIR/configs_enabled/*.toml
 
 export RUST_BACKTRACE=1
-cd ../node && cargo build --release
+cd ./piko/node && cargo build --release
 
-for f in $CONFIG
+for f in $CONFIG_FOLDER
 do
 	echo "Starting $f"
-	./target/release/main -p "$DIR/$f"
+	./target/release/main -p "$f"
 done
 
