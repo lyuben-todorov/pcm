0c8fca8ec5441233efd4ca76a75c286a485eab3b
Lyuben Todorov <lyuben.todorov@gmail.com>
2020-09-23 01:53:52 +0300

    Change release to debug

--- pcm.sh
+++ pcm.sh
@@ -5,11 +5,11 @@ REPO="git@github.com:lyuben-todorov/piko.git"
 CONFIG_FOLDER=$DIR/configs_enabled/*.toml
 
 export RUST_BACKTRACE=1
-cd ../node && cargo build --release
+cd ../node && cargo build --debug
 
 for f in $CONFIG_FOLDER
 do
 	echo "Starting $f"
-	./target/release/main -p "$f"
+	./target/debug/main -p "$f"
 done
 
